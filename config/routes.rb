8bafbfd7c500664c471cb691892d129b7c717ef3
Akasha <Akasha.Rojee@gmail.com>
2021-12-18 06:06:49 +0400

    Display random greeting

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static#index'
+
+  namespace :api, defaults: { format: 'json' } do
+    get 'greeting', to: 'greetings#index'
+  end
 end
