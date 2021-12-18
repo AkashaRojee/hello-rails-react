a9be11c47cf9f07b626a75c4547be46e690e1097
Akasha <Akasha.Rojee@gmail.com>
2021-12-18 06:36:26 +0400

    FFinalise app

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Greeting from "./Greeting";
+import store from "../redux/configureStore";
+import { Provider } from "react-redux";
 
 const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/greeting" element={<Greeting />} />
-    </Routes>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/greeting" element={<Greeting />} />
+        <Route
+          path="/"
+          element={<Link to="/greeting">Display random greeting</Link>}
+        />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
