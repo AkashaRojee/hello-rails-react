bb2f00ffbebfb3c8af5e3d1cbff9a27ec4cfc87f
Akasha <Akasha.Rojee@gmail.com>
2021-12-18 06:15:25 +0400

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'static#index'
 
   namespace :api, defaults: { format: 'json' } do
     get 'greeting', to: 'greetings#index'
   end
+
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
+
+  root 'static#index'
+
 
 end
